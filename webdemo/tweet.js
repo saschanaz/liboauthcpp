404a039577476f1d2959919e1cb3ac776f4b786f
SaschaNaz <saschanaz@outlook.com>
2015-02-26 16:59:34 +0900

    simple sample

--- webdemo/tweet.js
+++ webdemo/tweet.js
@@ -1,17 +1,44 @@
 /// <reference path="../ts-declaration/liboauthcpp.d.ts" />
+document.addEventListener("DOMContentLoaded", function () {
+    clearButton.addEventListener("click", function () {
+        while (tweetList.firstChild)
+            tweetList.removeChild(tweetList.firstChild);
+    });
+    streamStartButton.addEventListener("click", function () {
+        startStream().then(function () { return readStreamingTweets(); });
+    });
+    setClientButton.addEventListener("click", function () {
+        setClient(userKeyInput.value, userSecretInput.value);
+        userKeyInput.disabled = true;
+        userSecretInput.disabled = true;
+        setClientButton.disabled = true;
+    });
+});
 function httpRequest(method, url, options) {
     return new Promise(function (resolve, reject) {
         var xhr = new XMLHttpRequest();
-        xhr.onload = function () { return resolve(xhr.response); };
+        // responding state
+        if (typeof options.respondingState === "number") {
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === options.respondingState)
+                    resolve(xhr.response);
+            };
+        }
+        else {
+            xhr.onload = function () { return resolve(xhr.response); };
+        }
         xhr.onerror = function (err) { return reject(err); };
         xhr.open(method, url);
+        // response type
         if (options && typeof options.responseType === "string")
             xhr.responseType = options.responseType;
+        // headers
         if (options && "headers" in options) {
             for (var fieldname in options.headers) {
                 xhr.setRequestHeader(fieldname, options.headers[fieldname]);
             }
         }
+        // data
         if (options && "data" in options)
             xhr.send(options.data);
         else
@@ -39,4 +66,46 @@ function refresh(content) {
             Authorization: client.getHttpHeader(Module.HttpRequestType.Get, "" + endpoint, "", false)
         }
     });
+}
+var stream;
+function startStream() {
+    var endpoint = "https://userstream.twitter.com/1.1/user.json";
+    return httpRequest("GET", endpoint, {
+        headers: {
+            Authorization: client.getHttpHeader(Module.HttpRequestType.Get, "" + endpoint, "", false)
+        },
+        responseType: "ms-stream",
+        respondingState: 3
+    }).then(function (response) {
+        stream = response;
+        return stream;
+    });
+}
+function readStream(count) {
+    if (count === void 0) { count = 1000; }
+    return new Promise(function (resolve, reject) {
+        var reader = new MSStreamReader();
+        reader.onload = function () { return resolve(reader.result); };
+        reader.onerror = function (err) { return reject(err); };
+        reader.readAsText(stream, "UTF-8", count);
+    });
+}
+function readStreamingTweets() {
+    var buffer = "";
+    var readOnce = function () { return readStream().then(function (content) {
+        var end = content.indexOf("\r");
+        if (end !== -1) {
+            output(buffer + content.slice(0, end));
+            buffer = content.slice(end + 1);
+        }
+        else
+            buffer += content;
+        readOnce();
+    }); };
+    readOnce();
+}
+function output(content) {
+    var div = document.createElement("div");
+    div.textContent = content;
+    tweetList.appendChild(div);
 }
--- webdemo/tweet.ts
+++ webdemo/tweet.ts
@@ -1,27 +1,65 @@
 /// <reference path="../ts-declaration/liboauthcpp.d.ts" />
 
+declare var clearButton: HTMLInputElement;
+declare var streamStartButton: HTMLInputElement;
+declare var setClientButton: HTMLInputElement;
+declare var userKeyInput: HTMLInputElement;
+declare var userSecretInput: HTMLInputElement;
+declare var tweetList: HTMLDivElement;
+
+document.addEventListener("DOMContentLoaded", () => {
+  clearButton.addEventListener("click", () => {
+    while (tweetList.firstChild)
+      tweetList.removeChild(tweetList.firstChild);
+  });
+  streamStartButton.addEventListener("click", () => {
+    startStream().then(() => readStreamingTweets());
+  });
+  setClientButton.addEventListener("click", () => {
+    setClient(userKeyInput.value, userSecretInput.value);
+    userKeyInput.disabled = true;
+    userSecretInput.disabled = true;
+    setClientButton.disabled = true;
+  });
+});
+
 interface HttpRequestOptions {
   headers?: any;
   responseType?: any;
   data?: any;
+  respondingState?: number;
 }
 
 function httpRequest(method: string, url: string, options?: HttpRequestOptions) {
-  return new Promise<string>((resolve, reject) => {
+  return new Promise<any>((resolve, reject) => {
     var xhr = new XMLHttpRequest();
-    xhr.onload = () => resolve(xhr.response);
+
+    // responding state
+    if (typeof options.respondingState === "number") {
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === options.respondingState)
+          resolve(xhr.response)
+      };
+    }
+    else {
+      xhr.onload = () => resolve(xhr.response);
+    }
+
     xhr.onerror = (err) => reject(err);
     xhr.open(method, url);
 
+    // response type
     if (options && typeof options.responseType === "string")
       xhr.responseType = options.responseType;
 
+    // headers
     if (options && "headers" in options) {
       for (var fieldname in options.headers) {
         xhr.setRequestHeader(fieldname, options.headers[fieldname]);
       }
     }
 
+    // data
     if (options && "data" in options)
       xhr.send(options.data);
     else
@@ -52,4 +90,50 @@ function refresh(content: string) {
       Authorization: client.getHttpHeader(Module.HttpRequestType.Get, `${endpoint}`, "", false)
     }
   });
+}
+
+var stream: MSStream;
+function startStream() {
+  var endpoint = "https://userstream.twitter.com/1.1/user.json";
+  return httpRequest("GET", endpoint, {
+    headers: {
+      Authorization: client.getHttpHeader(Module.HttpRequestType.Get, `${endpoint}`, "", false)
+    },
+    responseType: "ms-stream",
+    respondingState: 3
+  }).then((response) => {
+    stream = response;
+    return stream;
+  });
+}
+
+function readStream(count = 1000) {
+  return new Promise<string>((resolve, reject) => {
+    var reader = new MSStreamReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (err) => reject(err);
+    reader.readAsText(stream, "UTF-8", count);
+  });
+}
+
+function readStreamingTweets() {
+  var buffer = "";
+  var readOnce = () => readStream().then((content) => {
+    var end = content.indexOf("\r");
+    if (end !== -1) {
+      output(buffer + content.slice(0, end));
+      buffer = content.slice(end + 1);
+    }
+    else
+      buffer += content;
+
+    readOnce();
+  });
+  readOnce();
+}
+
+function output(content: string) {
+  var div = document.createElement("div");
+  div.textContent = content;
+  tweetList.appendChild(div);
 }
