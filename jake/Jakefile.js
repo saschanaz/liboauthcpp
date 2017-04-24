c495d4764c5363fe50aa1302b89743e4e8eb0a7d
Kagami Sascha Rosylight <saschanaz@outlook.com>
2017-04-25 04:03:42 +0900

    maintaining + auto adding license

--- jake/Jakefile.js
+++ jake/Jakefile.js
@@ -1,65 +1,73 @@
-/// <reference path="ts-definitions/node-0.10.d.ts" />
-/// <reference path="ts-definitions/jake.d.ts" />
-var fs = require("fs");
-var cc = "emcc";
-var flags = '-O2 --memory-init-file 0 -s NO_FILESYSTEM="1" -s NO_BROWSER="1" -s MODULARIZE="1" -s EXPORT_NAME="_liboauthcpp" -std=c++11 -I ../include -D SHA1_LITTLE_ENDIAN';
-var sourceDirectory = "../src";
-var subcomponents = ["base64", "HMAC_SHA1", "SHA1", "urlencode"];
-var lib = "liboauthcpp";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mz = require("mz/fs");
+const jakeExecOptionBag = {
+    printStdout: true,
+    printStderr: true
+};
+function asyncExec(cmds) {
+    return new Promise((resolve, reject) => {
+        try {
+            jake.exec(cmds, () => resolve(), jakeExecOptionBag);
+        }
+        catch (e) {
+            reject(e);
+        }
+    });
+}
+const cc = "emcc";
+const flags = '-O2 --memory-init-file 0 -s NO_FILESYSTEM="1" -s MODULARIZE="1" -s EXPORT_NAME="_liboauthcpp" -std=c++11 -I ../include -D SHA1_LITTLE_ENDIAN';
+const sourceDirectory = "../src";
+const subcomponents = ["base64", "HMAC_SHA1", "SHA1", "urlencode"];
+const lib = "liboauthcpp";
 function subcomponentFiles(fileExtension) {
-    return subcomponents.map(function (item) { return (item + "." + fileExtension); });
+    return subcomponents.map(item => `${item}.${fileExtension}`);
 }
 //function nodeBtoa(input: string) {
 //  return new Buffer('' + input, 'binary').toString("base64");
 //}
 function definingCommand(definitions) {
-    var subcommands = [];
-    for (var key in definitions)
-        subcommands.push("-D " + key + "=\"" + definitions[key] + "\"");
+    const subcommands = [];
+    for (const key in definitions) {
+        subcommands.push(`-D ${key}="${definitions[key]}"`);
+    }
     return subcommands.join(' ');
 }
 desc("Compiles subcomponents");
-task("subcomponent", function () {
-    var files = subcomponentFiles("cpp");
-    var count = 0;
-    files.forEach(function (item) {
-        jake.exec([(cc + " " + flags + " -c " + sourceDirectory + "/" + item)], { printStdout: true, printStderr: true }, function () {
-            count++;
-            if (count === files.length)
-                complete();
-        });
-    });
-}, { async: true });
+task("subcomponent", async () => {
+    const files = subcomponentFiles("cpp");
+    await Promise.all(files.map(item => asyncExec([`${cc} ${flags} -c ${sourceDirectory}/${item}`])));
+});
 desc("Compiles liboauthcpp.cpp");
-task("lib", function () {
-    if (!fs.existsSync("apptoken.json")) {
+task("lib", async () => {
+    if (!await mz.exists("apptoken.json")) {
         throw new Error("Please give apptoken.json as a key-value object. ( { key: YOURKEY, secret: YOURSECRET } )");
     }
-    var apptoken;
+    let apptoken;
     try {
-        apptoken = JSON.parse(fs.readFileSync("apptoken.json").toString());
+        apptoken = JSON.parse(await mz.readFile("apptoken.json", "utf8"));
     }
     catch (e) {
         throw new Error("Incorrect json format in apptoken.json file.");
     }
-    if (!apptoken.key || !apptoken.secret)
+    if (!apptoken.key || !apptoken.secret) {
         throw new Error("apptoken.json must have `key` and `secret` property.");
+    }
     console.log("Jake received key and secret");
-    var definitions = { CONSUMER_KEY: apptoken.key, CONSUMER_SECRET: apptoken.secret };
+    const definitions = { CONSUMER_KEY: apptoken.key, CONSUMER_SECRET: apptoken.secret };
     //if ("buildNonce" in params) {
     //  definitions["BUILD_NONCE"] = nodeBtoa(Math.random().toString());
     //}
     //else {
     //  console.log("No buildNonce parameter is detected. Tokens will be untouched.");
     //}
-    jake.exec([(cc + " --bind " + flags + " -c " + sourceDirectory + "/" + lib + ".cpp " + definingCommand(definitions))], { printStdout: true, printStderr: true }, function () {
-        complete();
-    });
-}, { async: true });
+    await asyncExec([`${cc} --bind ${flags} -c ${sourceDirectory}/${lib}.cpp ${definingCommand(definitions)}`]);
+});
 desc("Builds a JavaScript OAuth component.");
-task("default", ["subcomponent", "lib"], function () {
-    jake.exec([(cc + " --bind " + flags + " -o " + lib + ".js " + lib + ".o " + subcomponentFiles("o").join(' '))], { printStdout: true, printStderr: true }, function () {
-        console.log("liboauthcpp is built successfully.");
-        complete();
-    });
-}, { async: true });
+task("default", ["subcomponent", "lib"], async () => {
+    await asyncExec([`${cc} --bind ${flags} -o ${lib}.js ${lib}.o ${subcomponentFiles("o").join(' ')}`]);
+    const compiled = await mz.readFile(`${lib}.js`, "utf8");
+    const license = await mz.readFile("../LICENSE", "utf8");
+    await mz.writeFile(`${lib}.js`, `/*\n${license}*/\n\n${compiled}`);
+    console.log("liboauthcpp is built successfully.");
+});
--- jake/Jakefile.ts
+++ jake/Jakefile.ts
@@ -1,15 +1,28 @@
-/// <reference path="ts-definitions/node-0.10.d.ts" />
-/// <reference path="ts-definitions/jake.d.ts" />
+import * as mz from "mz/fs";
 
-import fs = require("fs");
+const jakeExecOptionBag = {
+  printStdout: true,
+  printStderr: true
+};
 
-var cc = "emcc";
-var flags = '-O2 --memory-init-file 0 -s NO_FILESYSTEM="1" -s NO_BROWSER="1" -s MODULARIZE="1" -s EXPORT_NAME="_liboauthcpp" -std=c++11 -I ../include -D SHA1_LITTLE_ENDIAN';
-var sourceDirectory = "../src";
-var subcomponents = ["base64", "HMAC_SHA1", "SHA1", "urlencode"];
-var lib = "liboauthcpp";
+function asyncExec(cmds: string[]) {
+  return new Promise((resolve, reject) => {
+    try {
+      jake.exec(cmds, () => resolve(), jakeExecOptionBag)
+    }
+    catch (e) {
+      reject(e);
+    }
+  });
+}
+
+const cc = "emcc";
+const flags = '-O2 --memory-init-file 0 -s NO_FILESYSTEM="1" -s MODULARIZE="1" -s EXPORT_NAME="_liboauthcpp" -std=c++11 -I ../include -D SHA1_LITTLE_ENDIAN';
+const sourceDirectory = "../src";
+const subcomponents = ["base64", "HMAC_SHA1", "SHA1", "urlencode"];
+const lib = "liboauthcpp";
 function subcomponentFiles(fileExtension: string) {
-  return subcomponents.map((item) => `${item}.${fileExtension}`);
+  return subcomponents.map(item => `${item}.${fileExtension}`);
 }
 
 //function nodeBtoa(input: string) {
@@ -17,61 +30,57 @@ function subcomponentFiles(fileExtension: string) {
 //}
 
 function definingCommand(definitions: { [key: string]: string }) {
-  var subcommands: string[] = [];
-  for (var key in definitions)
+  const subcommands: string[] = [];
+  for (const key in definitions) {
     subcommands.push(`-D ${key}="${definitions[key]}"`);
+  }
 
   return subcommands.join(' ');
 }
 
 desc("Compiles subcomponents");
-task("subcomponent", function () {
-  var files = subcomponentFiles("cpp");
-  var count = 0;
-  files.forEach((item) => {
-    jake.exec([`${cc} ${flags} -c ${sourceDirectory}/${item}`], { printStdout: true, printStderr: true }, function () {
-      count++;
-      if (count === files.length)
-        complete();
-    });
-  });
-}, { async: true });
+task("subcomponent", async () => {
+  const files = subcomponentFiles("cpp");
+  await Promise.all(files.map(item => asyncExec([`${cc} ${flags} -c ${sourceDirectory}/${item}`])));
+});
 
 desc("Compiles liboauthcpp.cpp");
-task("lib", function () {
-  if (!fs.existsSync("apptoken.json")) {
+task("lib", async () => {
+  if (!await mz.exists("apptoken.json")) {
     throw new Error("Please give apptoken.json as a key-value object. ( { key: YOURKEY, secret: YOURSECRET } )");
   }
 
-  var apptoken: { key: string; secret: string };
+  let apptoken: { key: string; secret: string };
   try {
-    apptoken = JSON.parse(fs.readFileSync("apptoken.json").toString())
+    apptoken = JSON.parse(await mz.readFile("apptoken.json", "utf8"))
   }
   catch (e) {
     throw new Error("Incorrect json format in apptoken.json file.");
   }
-  if (!apptoken.key || !apptoken.secret)
+  if (!apptoken.key || !apptoken.secret) {
     throw new Error("apptoken.json must have `key` and `secret` property.");
-  
+  }
+
   console.log("Jake received key and secret");
 
-  var definitions: { [key: string]: string } = { CONSUMER_KEY: apptoken.key, CONSUMER_SECRET: apptoken.secret };
+  const definitions: { [key: string]: string } = { CONSUMER_KEY: apptoken.key, CONSUMER_SECRET: apptoken.secret };
   //if ("buildNonce" in params) {
   //  definitions["BUILD_NONCE"] = nodeBtoa(Math.random().toString());
   //}
   //else {
   //  console.log("No buildNonce parameter is detected. Tokens will be untouched.");
   //}
-  
-  jake.exec([`${cc} --bind ${flags} -c ${sourceDirectory}/${lib}.cpp ${definingCommand(definitions) }`], { printStdout: true, printStderr: true }, function () {
-    complete();
-  });
-}, { async: true });
+
+  await asyncExec([`${cc} --bind ${flags} -c ${sourceDirectory}/${lib}.cpp ${definingCommand(definitions)}`]);
+});
 
 desc("Builds a JavaScript OAuth component.");
-task("default", ["subcomponent", "lib"], function () {
-  jake.exec([`${cc} --bind ${flags} -o ${lib}.js ${lib}.o ${subcomponentFiles("o").join(' ')}`], { printStdout: true, printStderr: true }, function () {
-    console.log("liboauthcpp is built successfully.");
-    complete();
-  })
-}, { async: true });
+task("default", ["subcomponent", "lib"], async () => {
+  await asyncExec([`${cc} --bind ${flags} -o ${lib}.js ${lib}.o ${subcomponentFiles("o").join(' ')}`]);
+  
+  const compiled = await mz.readFile(`${lib}.js`, "utf8");
+  const license = await mz.readFile("../LICENSE", "utf8");
+  
+  await mz.writeFile(`${lib}.js`, `/*\n${license}*/\n\n${compiled}`);
+  console.log("liboauthcpp is built successfully.");
+});
